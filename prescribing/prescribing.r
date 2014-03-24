f0f6c78884444c335f1b0bb23f3994838bbd852c
mupini <ranie@ransomempini.com>
2014-03-24 21:50:25 +0000

    return data as one df

--- prescribing/prescribing.r
+++ prescribing/prescribing.r
@@ -2,20 +2,19 @@
 setwd("~/Documents/mpini/github/health/prescribing")
 library(sqldf)
 
-getStatinsData <- function(filename) {
-  print(paste('Loading in', filename))
-  data <- read.csv(paste('data', filename, sep='/'))
-  print(paste('Extracting statins entries from', filename))
-  return(sqldf('SELECT * FROM data WHERE LOWER(BNF_NAME) LIKE "%statin%"'))
+get_all_data <- function(medication) {
+  output_df <- data.frame()
+  for(filename in list.files(path='data', pattern='*.CSV')) {
+    print(paste('Loading in', filename))
+    tmp_data <- read.csv(paste('data', filename, sep='/'))
+    print(paste('Extracting statins entries from', filename))
+    tmp_data <- sqldf(paste('SELECT * FROM data WHERE LOWER(BNF_NAME) LIKE "%', medication, '%"', sep=''))
+    output_df <- rbind(output_df, tmp_data)
+  }
+  return(output_df)
 }
 
-statins <- data.frame()
-for(filename in list.files(path='data', pattern='*.CSV')) {
-  statins <- rbind(statins, getStatinsData(filename))
-  rm(filename)
-}
-
-head()
+statins <- get_all_data('statin')
 
 for(month in c('08', '09', '10', '11')) {
   data <- sqldf(paste('select * from statins where PERIOD = "2013', month, '"', sep=''))
@@ -46,28 +45,7 @@ sqldf('SELECT * FROM practice_data WHERE V2 IN(select PRACTICE from statins wher
 nhs.prescribing <- read.csv(path='data', 'prescribing.csv')
 colnames(nhs.prescribing) <- c(gsub('\\.', '_', colnames(nhs.prescribing)))
 
-nhs_prescribing <- nhs.prescribing
-statins <- sqldf('SELECT *
-                      FROM nhs_prescribing
-                      WHERE LOWER(BNF_NAME) LIKE "%statin%"')
-names(prescribing)
-head(statins)
-unique(statins$BNF_NAME)
-
-unique_names <- sqldf('SELECT DISTINCT BNF_NAME
-                        FROM statins
-                        ORDER BY BNF_NAME ASC')
-unique_names
-# get counts for each statin
-monthly_statin_count <- sqldf('SELECT BNF_NAME, count(*) 
-                              FROM statins
-                              GROUP BY BNF_NAME
-                              ORDER BY count(*) DESC')
-
-
-View(monthly_statin_count)
-
-
+# random scripts
 rans5 <- within(rans, {
   rans.j <- (rans.x + 2) * rans.y 
   m <- mean(rans.j)
@@ -83,9 +61,3 @@ hist.with.normal <- function(x, xlab=deparse(substitute(x)),...) {
    hist(x, freq=F, ylim=ylim, xlab=xlab, ...)
    curve(dnorm(x,m,s), add=T) }
 hist.with.normal(rnorm(100))
-
-
-testfunc <- function (x) {
-  return((max(x) - min(x)) / mean(x))
-}
-merged$diff <- apply(merged[,2:5], 1, testfunc)
